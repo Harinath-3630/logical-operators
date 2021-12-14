73849586016d0340ff85a06dc98750edb52771ce
Harinath <kathiarun3630@gmail.com>
2021-12-14 20:09:25 +0530

    logical operators

---
+++ index.js
@@ -0,0 +1,19 @@
+if(10<20 && 20<30 && 30<40){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+if(10<20 || 20<30 || 30<40){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+if(10>20) {
+    console.log("wrong statement");
+} else if (10<20){
+    console.log("right statement");
+}else{
+    console.log("choose another statement");
+}
